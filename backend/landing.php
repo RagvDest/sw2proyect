804b640552a138fa489be097988fc04302e706bd
jorgez93 <rennygorozabel@hotmail.com>
2018-12-19 22:13:46 -0500

    Añadiendo Header y Footer de Usuario

--- backend/landing.php
+++ backend/landing.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 
-<a class="btn btn-default btn-lg" href="#" role="button">Clientes</a>
+<a class="btn btn-default btn-lg" href="customer/customer_add.php" role="button">Clientes</a>
 <a class="btn btn-default btn-lg" href="#" role="button">Géneros</a>
 <a class="btn btn-default btn-lg" href="#" role="button">Películas</a>
 <a class="btn btn-default btn-lg" href="#" role="button">Proyecciones</a>
