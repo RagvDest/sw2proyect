15264d575ed8ebdaf56987369e092b4db674e079
Edgar David Guamán <edgardavid2015@users.noreply.github.com>
2019-01-10 03:39:11 -0500

    Creación del módulo películas
    Se realizó la implementación de los métodos insertar, eliminar y actualizar una película

--- backend/landing.php
+++ backend/landing.php
@@ -3,7 +3,7 @@
 
 <a class="btn btn-default btn-lg" href="customer/customer_add.php" role="button">Clientes</a>
 <a class="btn btn-default btn-lg" href="genre/genre_add.php" role="button">Géneros</a>
-<a class="btn btn-default btn-lg" href="#" role="button">Películas</a>
+<a class="btn btn-default btn-lg" href="movie/movie.php" role="button">Películas</a>
 <a class="btn btn-default btn-lg" href="showing/showing_add.php" role="button">Proyecciones</a>
 <a class="btn btn-default btn-lg" href="theatre/theatre.php" role="button">Cine</a>
 
