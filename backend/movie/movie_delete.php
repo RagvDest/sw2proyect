cbdb693b714a57585259136c8936257178a9cab1
Edgar David Guamán <edgardavid2015@users.noreply.github.com>
2019-01-17 12:06:34 -0500

    Iteración 4 - Solución en el módulo películas
    Agregué todos los .php de nuevo, espero ya no den problemas

--- backend/movie/movie_delete.php
+++ backend/movie/movie_delete.php
@@ -6,12 +6,28 @@ if(isset($_POST['SubmitMovieDelete'])) { //check if form was submitted
 	$result = $mysqli->query("DELETE FROM Movie WHERE MovieID=" . $_POST['MovieID']);
 	echo "<meta http-equiv='refresh' content='0'>";
 }
+
+if(isset($_POST['search'])){
+        $valueToSearch=$_POST['myInput'];
+        $qry="select * from movie where MovieName LIKE '%".$valueToSearch."%'";
+        $search_result=filterTable($mysqli,$qry);
+    }
+    else{
+        $qry="Select * from Movie";
+        $search_result=filterTable($mysqli,$qry);
+    }
+
+    function filterTable($mysqli,$qry){
+        $filter_result = $mysqli->query($qry);
+        return $filter_result;
+    }
+
 ?>
 
-<?php $result = $mysqli->query("SELECT * FROM Movie"); ?>
 
 <form action="" method="post">
-	<div class="col-md-11">
+	<div class="col-md-4"></div>
+	<div class="col-md-4">
 		<div class="row">
 			<br>
 			<h1>Eliminar película</h1>
@@ -22,22 +38,27 @@ if(isset($_POST['SubmitMovieDelete'])) { //check if form was submitted
 					<th>Película</th>
 				</tr>
 				<tr>
+					<th><input type="text" style="width: 70%;border-radius: 5px;" name="myInput" name="valueToSearch" placeholder="Buscar Peliculas..." title="Buscar Pelicula"></th>
+					<th><input type="submit" name="search" value="Filtrar"></th>
+				</tr>
+				<tr>
 					<td>
 						<select class="btn btn-default dropdown-toggle" type="button"  name="MovieID">
-							<?php while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ ?>
+							<?php while($row = mysqli_fetch_array($search_result, MYSQLI_ASSOC)){ ?>
 								<option value="<?php echo $row['MovieID'] ?>"><?php echo $row['MovieID'] . " - " . $row['MovieName'] ?></option>
 							<?php } ?>
 						</select>
 					</td>
 				</tr>
+				<tr><td></td><td><button type="submit" class="btn btn-default btn-lg" name="SubmitMovieDelete">Borrar</button></td></tr>
 			</table>
 		</div>
 	</div>
 	<div class="col-md-2">
-		<button type="submit" class="btn btn-default btn-lg" name="SubmitMovieDelete">Borrar</button>
+		
 	</div>
 </form>
 
-<?php $result->close(); ?>
+<?php $search_result->close(); ?>
 
 <?php include '../templatemovie/footer.php'; ?>
--- backend/movie/movie_list.php
+++ backend/movie/movie_list.php
@@ -1,6 +1,29 @@
+<?php include '../templatemovie/header.php' ?>
 <h1>Lista de películas</h1>
 
-<?php $result = $mysqli->query("SELECT * FROM Movie"); ?>
+<?php
+    if(isset($_POST['search'])){
+        $valueToSearch=$_POST['myInput'];
+        $qry="select * from movie where MovieName LIKE '%".$valueToSearch."%'";
+        $search_result=filterTable($mysqli,$qry);
+    }
+    else{
+        $qry="Select * from Movie";
+        $search_result=filterTable($mysqli,$qry);
+    }
+
+    function filterTable($mysqli,$qry){
+        $filter_result = $mysqli->query($qry);
+        return $filter_result;
+    }
+
+
+?>
+
+
+<form action="" method="post">
+<input type="text" style="width: 50%;border-radius: 5px;" name="myInput" name="valueToSearch" placeholder="Buscar Peliculas..." title="Buscar Pelicula">
+    <input type="submit" name="search" value="Filtrar">
 
 <table class="table table-striped">
     <tr>
@@ -8,7 +31,7 @@
         <th>ReleaseYear</th>
         <th>Genre</th>
     </tr>
-    <?php while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ ?>
+    <?php while($row = mysqli_fetch_array($search_result, MYSQLI_ASSOC)){ ?>
         <?php
         $result2 = $mysqli->query("SELECT GenreID FROM GenreMap WHERE MovieID=" . $row['MovieID']);
         $value2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
@@ -22,5 +45,6 @@
         </tr>
     <?php } ?>
 </table>
+</form>
 
-<?php $result->close(); ?>
+<?php $search_result->close(); ?>
